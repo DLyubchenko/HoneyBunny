96ce7e1222bd232b3d9e93afdc66a6a7b8aea7c0
roland_top <roland_top@ukr.net>
2017-12-19 03:34:17 +0200

    Add unit-tests project, add dummy test

---
+++ HoneyBunny.Tests/HomeControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using HoneyBunny.Controllers;
+using HoneyBunny.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace HoneyBunny.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        Mock<IRepository> _repoMock = new Mock<IRepository>();
+
+        [TestInitialize]
+        public void Init()
+        {
+            var categoryList = new List<Category>();
+
+            _repoMock.Setup(r => r.CategoryListAsync()).Returns(Task.FromResult(categoryList));
+        }
+
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController(_repoMock.Object);
+
+            // Act
+            var result = controller.Index();
+            var viewResult = result as Task<ActionResult>;
+            //var resModel = viewResult.Model as IEnumerable<Category>;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(viewResult);
+            //Assert.AreEqual("Index", viewResult.ViewName);
+            //Assert.IsNotNull(resModel);
+            //CollectionAssert.AreEqual(new[] { "Toys", "Planes" }, resModel.Select(i => i.Name).ToArray());
+        }
+    }
+}
---
+++ HoneyBunny.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HoneyBunny.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("HoneyBunny.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eae4531e-6c0f-4498-a306-2ad5bb9d9961")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
